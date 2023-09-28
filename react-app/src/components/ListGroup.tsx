268b88a422d960e7eb7cd28adea3bc7287b87827
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2023-09-28 10:00:50 -0400

    Created List Component
    Download bootstrap.
    
    Created List Group Component.
    
    Deleted template styling

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Fragment } from "react";
+
+function ListGroup() {
+    const items = [
+
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+
+
+    return (
+
+        <Fragment>
+            <h1>List</h1>
+            <ul className="list-group">
+                {items.map(item => <li>{item}</li>)};
+            </ul>
+        </Fragment>
+    );
+
+}
+
+export default ListGroup;
