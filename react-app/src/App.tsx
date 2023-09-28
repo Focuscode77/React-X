0e6cf09afb204e7614b724ad1d6468c8d23bbcc9
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2023-09-28 15:46:37 -0400

    Updated List group

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,10 +3,21 @@ import ListGroup from './components/ListGroup';
 
 
 function App() {
+  let items = [
 
-  return <div> <ListGroup /></div>
+    'New York',
+    'San Francisco',
+    'Tokyo',
+    'London',
+    'Paris'
+  ];
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+
+  }
+  return <div> <ListGroup items={items} heading='Cities' onSelectItem={handleSelectedItem} /></div>
 
 
-}
+};
 
 export default App
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,24 +1,43 @@
 import React from "react";
 import { Fragment } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-    const items = [
 
-        'New York',
-        'San Francisco',
-        'Tokyo',
-        'London',
-        'Paris'
-    ];
+interface Props {
+
+    items: string[];
+    heading: string;
+    // (item : string)
+    onSelectItem: (item: string) => void;
+
+
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+
+
+    //Hook
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
 
 
 
     return (
 
         <Fragment>
-            <h1>List</h1>
-            <ul className="list-group">
-                {items.map(item => <li>{item}</li>)};
+            <h1>{heading}</h1>
+            {items.length === 0 && <p> No item found</p>}
+
+            <ul className="list-group ">
+                {items.map((item, index) => (
+                    <li
+                        className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+                        key={item}
+                        onClick={() => { setSelectedIndex(index); onSelectItem(item); }}
+                    >
+                        {item}
+                    </li>
+                ))};
             </ul>
         </Fragment>
     );
