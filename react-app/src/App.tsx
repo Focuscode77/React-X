1da3ea75724295e08d948158a9e9e0d620fd41b7
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2023-10-03 15:50:01 -0400

    Form Update

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import Expandable from './components/Expandable'
+import Form from './components/Form'
 import { useState } from 'react';
 
 
@@ -10,8 +10,7 @@ function App() {
   return (
 
     <div>
-      <Expandable>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus facilis neque fugit dolores incidunt omnis asperiores repellendus tenetur fugiat ut iure autem, voluptas unde sequi necessitatibus in nulla itaque corrupti quidem deserunt. Animi cupiditate exercitationem eaque aliquam at odio reiciendis, obcaecati a dolore aut et error repudiandae quos nihil autem? Necessitatibus quasi vel dicta ea quis quos rem cumque! Dicta quae commodi magni, impedit aspernatur velit rem neque ipsum. Impedit magni et quam ipsa nam consequatur, odio, reiciendis accusantium voluptas dolorum cupiditate officiis culpa in blanditiis! Necessitatibus, a assumenda consequuntur id consectetur aliquam, dolore, sint eaque adipisci quisquam repudiandae!</Expandable>
-
+      <Form />
     </div>
 
 
