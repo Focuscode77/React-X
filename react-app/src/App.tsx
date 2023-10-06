a44ccd7a44b3474ebba7686472ac8ba3c6b51a36
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2023-10-06 17:00:13 -0400

    Error handling
    Implementation of error handling and zod

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react';
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import categories from "./expense-tracker/categories"
 
 
 function App() {
@@ -26,7 +26,7 @@ function App() {
     <div>
       <div>
         <div className="mb-5">
-          <ExpenseForm />
+          <ExpenseForm onSubmit={expense => setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])} />
         </div>
       </div>
       <div className="mb-3">
--- react-app/src/expense-tracker/components/ExpenseForm.tsx
+++ react-app/src/expense-tracker/components/ExpenseForm.tsx
@@ -1,24 +1,63 @@
 import React from 'react'
-import { categories } from '../../App';
-const ExpenseForm = () => {
+import categories from "../categories"
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const schema = z.object({
+
+    description: z.string().min(3).max(50),
+    amount: z.number({ invalid_type_error: 'Amount is required' }).min(0.01, { message: 'Description should be atleast 3 characters' }).max(100_000),
+    category: z.enum(categories, {
+
+        errorMap: () => ({ message: 'Category is required.' })
+
+
+
+    })
+
+})
+
+type ExpenseFormData = z.infer<typeof schema>;
+
+
+interface Props {
+
+    onSubmit: (data: ExpenseFormData) => void;
+
+
+}
+
+const ExpenseForm = ({ onSubmit }: Props) => {
+    const { register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+    } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) })
     return (
-        <form>
+        <form onSubmit={handleSubmit(data => {
+            onSubmit(data);
+            reset();
+        })}>
 
             <div className="mb-3">
                 <label htmlFor="description" className="form-label">Description</label>
-                <input id="description" className="form-control"></input>
+                <input {...register('description')} id="description" className="form-control"></input>
+                {errors.description && <p className="text-danger">{errors.description.message}</p>}
             </div>
             <div className="mb-3"><label htmlFor="amount" className="form-label">Amount</label>
-                <input id="amount" type="number" className="form-control" />
+                <input {...register('amount', { valueAsNumber: true })} id="amount" type="number" className="form-control" />
+                {errors.amount && <p className="text-danger">{errors.amount.message}</p>}
             </div>
             <div className="mb-3"><label htmlFor="category" className="form-label">Category</label>
-                <select name="" id="category" className="form-select">
+                <select name=""{...register('category')} id="category" className="form-select">
                     <option value=""></option>
                     {categories.map(category => <option key={category} value={category}>{category}</option>)}
                 </select>
+                {errors.category && <p className="text-danger">{errors.category.message}</p>}
             </div>
             <button className="btn btn-primary">Submit</button>
-        </form>
+        </form >
     )
 }
 
